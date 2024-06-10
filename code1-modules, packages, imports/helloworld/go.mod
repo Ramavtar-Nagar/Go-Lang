014b4ef20ec69f4f4076df6c5b1c0d2b264b552e
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-06-11 04:00:23 +0530

    module imports in Go-Lang

---
+++ code1-modules, packages, imports/helloworld/go.mod
@@ -0,0 +1,7 @@
+module hello_world
+
+go 1.22.4
+
+replace myutil => ../myutil
+
+require myutil v0.0.0
---
+++ code1-modules, packages, imports/helloworld/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "myutil"
+)
+
+func main() {
+    fmt.Println("Starting the GO fortune...")
+    fmt.Println("_____________________________>")
+
+    myutil.PrintMessage("Hey, you are disturbing me...")
+}
