9817ff8d761b5ff91e068f1a32da46a83dfebd6d
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-06-13 03:08:33 +0530

    new verion of hello file

---
+++ code0-hello world/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"mylearning/myutil"
+)
+
+func main() {
+	fmt.Println("Starting the GO fortune...")
+	fmt.Println("_____________________________>")
+
+	myutil.PrintMessage("Hey, you are disturbing me...")
+}
